bad4891767eccbbf03446d960fceb636a86e6ad8
Alex Shrestha <alexshrestha1996@gmail.com>
2024-07-01 19:48:09 -0700

    created simple perceptron class

--- perceptron.py
+++ perceptron.py
@@ -1,6 +1,58 @@
 import numpy as np
 
 
+class Perceptron:
+    def __init__(self, X: np.ndarray, y: np.array):
+        self.X = X
+        self.y = y
+
+        # Adding bias column to each training data
+        ones_column = np.ones((X.shape[0], 1))
+
+        self.X = np.hstack((ones_column, self.X))
+
+        self.learning_rate = 0.1
+
+        # Initializing our weights to be all zeroes
+        # Size of weights is the number of rows in our input array
+        self.weights = np.zeros((self.X.shape[1], 1))
+
+
+    def fit(self):
+        print(f"Shape of self. X is {self.X.shape}")
+        print(f"Shape of self.weights is {self.weights.shape}")
+
+        predicted = self.X @ self.weights
+
+        update_weights = np.zeros((self.X.shape[1], 1))
+
+        # For each set of data (for each row)
+        for i in range(self.X.shape[0]):
+            # Checking if signs match
+            signed_arr = []
+            signed_arr.append(predicted[i])
+            signed_arr.append(self.y[i])
+
+            # sign_arr = np.sign([predicted[i], self.y[i]])
+            
+            # If our prediction is incorrect
+            if signed_arr[0] != signed_arr[1]:
+                # For each weight coefficient we calculate update value
+                # using stochastic gradient descent
+                for j in range(update_weights.shape[0]): 
+                    update_weights[j] = self.X[i, j] * (np.dot(self.X[i], self.weights) - self.y[i])
+           # If our prediction is correct, just set update_weights to all zeroes
+            else:
+                update_weights = 0 * update_weights
+
+            # Updating weights with whatever the learning_rate is
+            self.weights = self.weights - self.learning_rate * update_weights
+
+        return self.weights
+
+
+
+
 def ols(A: np.ndarray, b: np.array) -> np.ndarray:  # returns best fit line
     x = np.zeros(A.shape[0])
     A_t = np.linalg.matrix_transpose(A)
@@ -15,22 +67,17 @@ def ols(A: np.ndarray, b: np.array) -> np.ndarray:  # returns best fit line
 
 def main():
 
-    A = np.array([[3, 2], [1, 0]], np.int32)
-    b = np.ones(2)
-    
-    y = ols(A, b)
+    A = np.array([[2, 3], [1, 1],
+                  [2, 1], [1, 2]], np.int32)
+    b = np.array([1, -1, 1, -1])
 
-    print(y)
+    perceptron = Perceptron(A, b)
+    weights = perceptron.fit()
+
+    print(weights)
 
     return
 
 
-
-
 if __name__ == "__main__":
     main()
-
-    
-
-
-
