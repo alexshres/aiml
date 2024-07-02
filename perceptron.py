8b57e905813fd7b34e5d5dbeb49311928ddb5ec9
Alex Shrestha <alexshrestha1996@gmail.com>
2024-07-01 20:42:47 -0700

    updated and correct simple perceptron algorithm

--- perceptron.py
+++ perceptron.py
@@ -17,30 +17,38 @@ class Perceptron:
         # Size of weights is the number of rows in our input array
         self.weights = np.zeros((self.X.shape[1], 1))
 
+        self.iters = 100
+
 
     def fit(self):
         predicted = self.X @ self.weights
 
         update_weights = np.zeros((self.X.shape[1], 1))
+        
+        for i in range(self.iters):
+            indices = np.arange(self.X.shape[0])
+            np.random.shuffle(indices)
+            self.X = self.X[indices]
+            self.y = self.y[indices]
 
-        # For each set of data (for each row)
-        for i in range(self.X.shape[0]):
-            # Checking if signs match
-            predict_sign = np.sign(predicted[i])
-            actual_sign = np.sign(self.y[i])
+            # For each set of data (for each row)
+            for i in range(self.X.shape[0]):
+                # Checking if signs match
+                predict_sign = np.sign(predicted[i])
+                actual_sign = np.sign(self.y[i])
 
-            # If our prediction is incorrect
-            if predict_sign != actual_sign:
-                # For each weight coefficient we calculate update value
-                # using stochastic gradient descent
-                for j in range(update_weights.shape[0]): 
-                    update_weights[j] = self.X[i, j] * (np.dot(self.X[i], self.weights) - self.y[i])
-           # If our prediction is correct, just set update_weights to all zeroes
-            else:
-                update_weights = 0 * update_weights
+                # If our prediction is incorrect
+                if predict_sign != actual_sign:
+                    # For each weight coefficient we calculate update value
+                    # using stochastic gradient descent
+                    for j in range(update_weights.shape[0]): 
+                        update_weights[j] = self.X[i, j] * (np.dot(self.X[i], self.weights) - self.y[i])
+               # If our prediction is correct, just set update_weights to all zeroes
+                else:
+                    update_weights = 0 * update_weights
 
-            # Updating weights with whatever the learning_rate is
-            self.weights = self.weights - (self.learning_rate * update_weights)
+                # Updating weights with whatever the learning_rate is
+                self.weights = self.weights - (self.learning_rate * update_weights)
 
         return self.weights
 
@@ -62,7 +70,7 @@ def ols(A: np.ndarray, b: np.array) -> np.ndarray:  # returns best fit line
 def main():
 
     A = np.array([[2, 3], [1, 1],
-                  [2, 1], [1, 2]], np.int32)
+                  [2, 1], [1, 2]])
     b = np.array([1, -1, 1, -1])
 
     perceptron = Perceptron(A, b)
