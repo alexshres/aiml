798a16eb0693bc192d5fafaebe1488638cc8633a
Alex Shrestha <alexshrestha1996@gmail.com>
2024-07-05 12:14:10 -0700

    adding gz mnist files

---
+++ perceptrons/mnist_data.py
@@ -0,0 +1,13 @@
+import gzip
+import numpy as np
+import struct
+
+train_images_path = 'train-images-idx3-ubyte.gz'
+train_labels_path = 'train-labels-idx1-ubyte.gz'
+
+
+def load_data():
+    pass
+
+
+
