d0e2c15f652c78f9ef23e7b3c3f77a25d2aa78a9
Alex Shrestha <alexshrestha1996@gmail.com>
2024-07-19 12:28:44 -0700

    updated names

--- perceptrons/nn/main.py
+++ perceptrons/nn/main.py
@@ -1,70 +1,24 @@
 import two_layer_MLP as tm
 import mnist_data as md
 import numpy as np
-import mlp 
-
-# data paths
-train_images_path = './data/mnist_gz/train-images-idx3-ubyte.gz'
-train_labels_path = './data/mnist_gz/train-labels-idx1-ubyte.gz'
-
-train_data = md.load_images(train_images_path)
-train_labels = md.load_labels(train_labels_path)
-
-test_images_path = './data/mnist_gz/t10k-images-idx3-ubyte.gz'
-test_labels_path = './data/mnist_gz/t10k-labels-idx1-ubyte.gz'
-
-test_data = md.load_images(test_images_path)
-test_labels = md.load_labels(test_labels_path)
-
-def experiment_1():
-    for n in [20, 50, 100]:
-        model = tm.TwoMLP(train_data, train_labels, hidden_nodes=n,
-                          outputs=10, epochs=50, 
-                          learning_rate=0.1, momentum=0.9,
-                          type="hidden_nodes", type_value=n)
-
-        model.train(test_data, test_labels)
-        model.fit(test_data, test_labels)
-
-def experiment_2():
-    for m in [0, 0.25, 0.5]:
-        model = tm.TwoMLP(train_data, train_labels, hidden_nodes=100,
-                          outputs=10, epochs=50, 
-                          learning_rate=0.1, momentum=m,
-                          type="momentum", type_value=m)
-
-        model.train(test_data, test_labels)
-        model.fit(test_data, test_labels)
-
-
-def experiment_3():
-    for s in [0.25, 0.5]:
-        size = int(s * train_data.shape[0])
-
-        p = np.random.permutation(len(train_data))
-        new_train_data = train_data[p]
-        new_train_labels = train_labels[p]
-
-        new_train_data = new_train_data[:size, :] 
-        new_train_labels = new_train_labels[:size]
-
-            
-        model = tm.TwoMLP(train_data, train_labels, hidden_nodes=100,
-                          outputs=10, epochs=50, 
-                          learning_rate=0.1, momentum=0.9,
-                          type="training_size", type_value=size)
-
-        model.train(test_data, test_labels)
-        model.fit(test_data, test_labels)
+import nn 
 
 
 
 def main():
     """
-    experiment_1()
-    experiment_2()
-    experiment_3()
+    # data paths
+    train_images_path = './data/mnist_gz/train-images-idx3-ubyte.gz'
+    train_labels_path = './data/mnist_gz/train-labels-idx1-ubyte.gz'
 
+    train_data = md.load_images(train_images_path)
+    train_labels = md.load_labels(train_labels_path)
+
+    test_images_path = './data/mnist_gz/t10k-images-idx3-ubyte.gz'
+    test_labels_path = './data/mnist_gz/t10k-labels-idx1-ubyte.gz'
+
+    test_data = md.load_images(test_images_path)
+    test_labels = md.load_labels(test_labels_path)
     """
 
     # XOR example
@@ -74,7 +28,7 @@ def main():
     nodes = [2, 2, 2]
 
 
-    model = mlp.MLP(X, y, nodes=nodes)
+    model = nn.NeuralNetwork(X, y, nodes=nodes)
 
     model.train()
     
