3e263411d0c7898c2fa9d06fda277b2731141c5a
Alex Shrestha <alexshrestha1996@gmail.com>
2024-07-30 14:04:28 -0700

    outline for nbc class

---
+++ nbc/nbc.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+
+
+class NaiveBayes:
+    
+    def __init__(self):
+        pass
+
+    def __feature_stats(self):
+        pass
+
+    def __find_class_prob(self):
+        pass
+
+    def __get_priors(self):
+        pass
+
+    def train(self):
+        pass
+
+    def predict(self):
+        pass
