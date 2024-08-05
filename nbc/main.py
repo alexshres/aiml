604aaaaf88af2227f34095e1796db2a90534321e
Alex Shrestha <alexshrestha1996@gmail.com>
2024-08-05 12:53:54 -0700

    finished nbc

--- nbc/main.py
+++ nbc/main.py
@@ -3,7 +3,7 @@ import spam_data as sd
 import nbc
 
 from sklearn.model_selection import train_test_split
-
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score
 
 def main():
     # Naive Bayes classifier will take in a pandas dataframe object
@@ -14,7 +14,23 @@ def main():
                                             stratify=sd.target      # Ensures 60/40 split
                                         )
 
-    print(sd.features.columns)
+    classifier = nbc.NaiveBayesBinary(training_data=X_train, 
+                                      training_labels=y_train, 
+                                      num_classes=2)
+
+    
+    y_pred = classifier.predict(X_test)
+    targets = y_test['Class'].tolist()
+
+
+    accuracy = accuracy_score(targets, y_pred)
+    precision = precision_score(targets, y_pred)
+    recall = recall_score(targets, y_pred)
+    conf_matrix = confusion_matrix(targets, y_pred)
+
+
+    print(f"{accuracy=}\n{precision=}\n{recall=},\n{conf_matrix=}")
+
 
 
 if __name__ == "__main__":
--- nbc/nbc.py
+++ nbc/nbc.py
@@ -2,23 +2,56 @@ import numpy as np
 import pandas as pd
 
 
-class NaiveBayes:
+class NaiveBayesBinary:
+    """
+    NaiveBayesBinary class only works for binary classification currently
+    """
     
-    def __init__(self, training_data, training_labels):
+    def __init__(self, training_data, training_labels, num_classes):
         self.X = training_data
         self.y = training_labels
+        self.classes = num_classes
 
-    def __feature_stats(self):
-        pass
+        # Grabbing mean and standard deviation
+        self.combined_df = pd.concat([self.X, self.y], axis=1)
+        self.mean_cls = self.combined_df.groupby('Class').mean()
 
-    def __find_class_prob(self):
-        pass
+        # Updating std so no underflow errors
+        self.std_cls = self.combined_df.groupby('Class').std()
+        self.std_cls = self.std_cls.map(lambda x: 0.0001 if x < 0.0001 else x)
 
-    def __get_priors(self):
-        pass
+        # Calculating empirical class probabilities
+        p_one = self.y.mean()
+        p_two = 1 - p_one
+        
+        self.probabilities = [p_one, p_two]
 
-    def train(self):
-        pass
+    def __normal_dist(self, x, mean, std):
+        return (1.0 / (std * np.sqrt(2 * np.pi))) *  \
+                np.exp(-0.5 * ((x - mean) / std) ** 2)
 
     def predict(self, test_data):
-        pass
+        predictions = []
+
+        for index, row in test_data.iterrows():
+            # Creating likelihoods for each class for each feature
+            likelihoods = [0, 0]
+
+            for column in test_data.columns:
+                likelihoods[0] += np.log(self.__normal_dist(row[column], 
+                                                            self.mean_cls.loc[0, column], 
+                                                            self.std_cls.loc[0, column]))
+
+                likelihoods[1] += np.log(self.__normal_dist(row[column], 
+                                                            self.mean_cls.loc[1, column], 
+                                                            self.std_cls.loc[1, column]))
+
+            # Adding prior probabilities for each class
+            for i in range(self.classes):
+                likelihoods[i] += np.log(self.probabilities[i])
+
+            predictions.append(np.argmax(likelihoods))
+
+        return predictions
+
+
