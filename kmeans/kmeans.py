1b9b9a8abf8ce1d01a709505ac57082b0651d791
Alex Shrestha <alexshrestha1996@gmail.com>
2024-08-08 17:32:52 -0700

    added EM functions

--- kmeans/kmeans.py
+++ kmeans/kmeans.py
@@ -10,7 +10,8 @@ class KMeans:
         mins = self.X.min(axis=0).tolist()
         max = self.X.max(axis=0).tolist()
 
-        self.centers = [] 
+        # Initializing centers
+        self.__centers = [] 
 
         # Randomly initializing centers based on min/max of the data
         for c in range(self.k):
@@ -18,11 +19,30 @@ class KMeans:
             high = max[c]
             self.centers.append(np.random.uniform(low, high, self.X.shape[1]))
 
-        print(self.centers)
+        # Defines which cluster each data is assigned to
+        # The value is mapped to the index of the self.__centers 
+        self.__cluster = [-1] * self.X.shape[0]
 
+
+    def __closest_cluster(self, data):
+        """
+        Updates self.__cluster for each data point to the cluster that has the 
+        smalles Euclidean distance
+        """
+        pass
+
+    def __update_centers(self):
+        """
+        Updates the self.__centers based on how each data point is clustered
+        """
+        pass
 
 
     def train(self):
+        # TODO: how to show convergence
+        for row in range(self.X.shape[0]):
+            self.__closest_cluster(self.X[row])
+            self.__update_centers()
         pass
 
 
